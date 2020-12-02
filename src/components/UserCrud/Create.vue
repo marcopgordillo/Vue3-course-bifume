2b789905f66d6e21fbd182e203a89e55c7187432
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-12-02 10:56:30 -0500

    Fix tailwind purge

--- src/components/UserCrud/Create.vue
+++ src/components/UserCrud/Create.vue
@@ -1,29 +1,31 @@
 <template>
-  <button class="px-2 py-1 border rounded my-2" @click="isModalOpen = true">Add User</button>
-  <teleport to="body">
-    <Modal v-if="isModalOpen" @close="isModalOpen = false">
-      <template #title>
-        <h1 class="text-2xl">Add New User</h1>
-      </template>
-      <form @submit.prevent="submit">
-        <div class="p-2">
-          <label for="name">Name</label>
-          <input class="w-full p-2 rounded border" id="name" v-model="state.form.name" type="text" placeholder="User Name">
-        </div>
-        <div class="p-2">
-          <label for="name">Email</label>
-          <input class="w-full p-2 rounded border" id="email" v-model="state.form.email" type="email" placeholder="User Email">
-        </div>
-        <div class="p-2">
-          <label for="name">Avatar Url</label>
-          <input class="w-full p-2 rounded border" id="avatar" v-model="state.form.avatar" type="url" placeholder="User Avatar">
-        </div>
-        <div class="p-2">
-          <button class="w-full p-2 rounded border hover:bg-gray-300" type="submit">Create</button>
-        </div>
-      </form>
-    </Modal>
-  </teleport>
+  <div>
+    <button class="px-2 py-1 border rounded my-2" @click="isModalOpen = true">Add User</button>
+    <teleport to="body">
+      <Modal v-if="isModalOpen" @close="isModalOpen = false">
+        <template #title>
+          <h1 class="text-2xl">Add New User</h1>
+        </template>
+        <form @submit.prevent="submit">
+          <div class="p-2">
+            <label for="name">Name</label>
+            <input class="w-full p-2 rounded border" id="name" v-model="state.form.name" type="text" placeholder="User Name">
+          </div>
+          <div class="p-2">
+            <label for="name">Email</label>
+            <input class="w-full p-2 rounded border" id="email" v-model="state.form.email" type="email" placeholder="User Email">
+          </div>
+          <div class="p-2">
+            <label for="name">Avatar Url</label>
+            <input class="w-full p-2 rounded border" id="avatar" v-model="state.form.avatar" type="url" placeholder="User Avatar">
+          </div>
+          <div class="p-2">
+            <button class="w-full p-2 rounded border hover:bg-gray-300" type="submit">Create</button>
+          </div>
+        </form>
+      </Modal>
+    </teleport>
+  </div>
 </template>
 
 <script>
